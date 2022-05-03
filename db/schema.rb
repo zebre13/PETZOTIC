f9da92df926e5be947f0a2a0848ca41848da6b7e
Boris <95616326+zebre13@users.noreply.github.com>
2022-05-03 21:27:30 +0200

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_03_182026) do
+ActiveRecord::Schema.define(version: 2022_05_03_190833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,12 +43,24 @@ ActiveRecord::Schema.define(version: 2022_05_03_182026) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "pet_id", null: false
+    t.datetime "starting_date"
+    t.datetime "ending_date"
+    t.boolean "confirmed", default: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["pet_id"], name: "index_bookings_on_pet_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "pets", force: :cascade do |t|
     t.string "category"
     t.string "name"
     t.text "description"
     t.string "specie"
-    t.boolean "available"
+    t.boolean "available", default: true
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -72,5 +84,7 @@ ActiveRecord::Schema.define(version: 2022_05_03_182026) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "pets"
+  add_foreign_key "bookings", "users"
   add_foreign_key "pets", "users"
 end
