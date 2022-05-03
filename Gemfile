2a2f1c4fe12203134007ac225d38c63e557954cd
Etienne de Dianous <etiennededi@hotmail.fr>
2022-05-03 16:39:25 +0200

    Rajouté les gemmes nécessaires a Pundit et Active Storage. Ajouté le champ photo et prix à la table Pet. Création du form pour les new pet.

--- Gemfile
+++ Gemfile
@@ -26,6 +26,7 @@ gem 'jbuilder', '~> 2.7'
 # gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
+gem 'pundit'
 gem 'bootsnap', '>= 1.4.4', require: false
 gem 'devise'
 gem 'autoprefixer-rails', '10.2.5'
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :user, dependent: :destroy
+  has_many_attached :photos
+  validates :category, inclusion: { in: %w(categorie1 categorie2 categorie3 modifier_dans_pet.rb),
+    message: "%{value} is not a valid category" }
+  validates :specie, inclusion: {in: %w(espece1 espece2 espece3 modifier_dans_pet.rb),
+    message: "%{value} is not a valid specie"}
 end
