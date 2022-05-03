0aba6ec8a51b666d2123536a920acf970ce3c7e7
Ghita Aaddaj <aa.ghita@gmail.com>
2022-05-03 20:36:24 +0200

    seeds completed without photos

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,13 +1,13 @@
 class Pet < ApplicationRecord
-  CATEGORIES = %w(categorie1 categorie2 categorie3 modifier_dans_pet.rb)
-  SPECIES = %w(specie1 specie2 specie3 modifier_dans_pet.rb)
+  CATEGORIES = %w(mammal categorie2 categorie3 modifier_dans_pet.rb)
+  SPECIES = ['Mi-monkey, mi-man', 'Bengal tiger', 'African gorilla', 'Java Rhinoceros', 'Hirola']
   belongs_to :user, dependent: :destroy
   has_many_attached :photos
 
 
   validates :name, presence: true, uniqueness: true
   validates :specie, presence: true, inclusion: { in: SPECIES,
-    message: "%{value} is not a valid specie" }
+                                                  message: "%{value} is not a valid specie" }
   validates :category, inclusion: { in: CATEGORIES,
-    message: "%{value} is not a valid category" }
+                                    message: "%{value} is not a valid category" }
 end
