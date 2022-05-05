443f269101485d3b813c3ae52d1795c38478148e
Ghita Aaddaj <aa.ghita@gmail.com>
2022-05-05 21:41:28 +0200

    edit/update pet added

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :pets, only: %i[index show new create] do
+  resources :pets do
     member do
       get :booking
     end
   end
 end
+
+# only: %i[index show new create edit update]
