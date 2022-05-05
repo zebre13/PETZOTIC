eafa4a4179938180909d221942117f459bc374f9
Maria Varela <leonor.varela91330@gmail.com>
2022-05-05 21:20:24 +0200

    modifier la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+Pet.destroy_all
+User.destroy_all
 # users
 leonor = { username: 'leonor',
            address: '10 rue de paradis, 75010 Paris',
@@ -36,38 +37,41 @@ def seed_users(users)
   p 'Seeds Users saved'
 end
 
+seed_users(users)
+
 # pets
+created_users = User.all
 
 tarzan = { category: 'mammal',
            name: 'Tarzan',
            description: 'Tarzan is not very evolved. He descends from the monkey, the tree and the neighbour, but don\'t hold it against him. Unfortunately, he will not be able to show great subtlety, but will know how to defend your home and your wife, if you have one. If you don\'t have one, pretend to have one and it will make the animal very happy. It can smell a little strong sometimes, but we rent with it a tropical atmosphere mist to delicately embalm your interior.',
            specie: 'Mi-monkey, mi-man',
-           user_id: 1}
+           user: created_users[0]}
 
 
 roger = { category: 'mammal',
           name: 'Roger',
           description: "Soft and kind as a kitten, roger will go perfectly with your interior. He has a neat coat like the carpet and brown whiskers that will match very well with your curtains whatever their color. He doesn't eat much, at set times, of course. If he bothers you by making too much noise, we even have a small electric collar that will do the job of silence perfectly.",
           specie: 'Bengal tiger',
-          user_id: 2}
+          user: created_users[1]}
 
 jamila = { category: 'mammal',
            name: 'Jamila',
            description: 'Jamila is so to speak not very present. Not one to look for lice, it blends in with the decor, especially if you have tall trees around your home. She loves making jokes and eating bananas. But she also knows how to iron, do the housework, the dishes,... she really likes to mimic everything you do. If you have children, she can even do their homework. Easy as a monkey.',
            specie: 'African gorilla',
-           user_id: 2}
+           user: created_users[2]}
 
 carlos = { category: 'mammal',
            name: 'Carlos',
            description: "Very docile, Carlos will follow you everywhere. Some clutter issues sometimes, but after all it's not the size that counts. However, do not venture to take the metro with him, it could end in tragedy. Taupe gray color, and name rhinoceros, Carlos is an original. Very little talker",
            specie: 'Java Rhinoceros',
-           user_id: 3}
+           user: created_users[3]}
 
 nala = { category: 'mammal',
          name: 'Nala',
          description: 'Long brown horns, gazelle smile and doe eyes, Nala is present damn well. She will impress your in-laws with her good manners. You can also use its horns as coat racks, bag holders on the go or changing rooms at a party, if it runs out',
          specie: 'Hirola',
-         user_id: 4}
+         user: created_users[0]}
 
 pets = [tarzan, roger, jamila, carlos, nala]
 
@@ -92,8 +96,6 @@ def seed_pets(pets)
   p 'Seeds Pets saved'
 end
 
-
 # call methods seed
 
-seed_users(users)
 seed_pets(pets)
