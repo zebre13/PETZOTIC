f18eb45b6f4febeb7ad555454732dfe0bb33d919
Etienne de Dianous <etiennededi@hotmail.fr>
2022-05-08 11:52:16 +0200

    Added comments to uncomment on booking.rb

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,17 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :pet
   enum status: [ :pending, :accepted, :rejected ]
+  # validate :reservations_must_not_overlap
 
+  # private
+
+  # def reservations_must_not_overlap
+  #   return if self.class
+  #                 .where.not(id: id)
+  #                 .where(pet_id: pet_id)
+  #                 .where('starting_date < ? AND ending_date > ?', ending_date, starting_date)
+  #                 .none?
+  #   flash.alert = "Overlapping reservation exists."
+  #   errors.add(:base, 'Overlapping reservation exists')
+  end
 end
